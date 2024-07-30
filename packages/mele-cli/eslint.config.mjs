513f00213a159e6a4b2c0355c44d935103142d03
melelong <1137042726@qq.com>
2024-07-30 23:47:08 +0800

    test

---
+++ packages/mele-cli/eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: {
+      pluginPrettier: pluginPrettier
+    }
+  }
+]
